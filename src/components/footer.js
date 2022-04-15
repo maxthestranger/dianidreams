336804b052bebcabe25ca777cb7957691bf98e25
maxthestranger <thestrangermax@gmail.com>
2022-04-15 18:46:18 +0300

    remove bootstrap icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import 'boxicons';
+import * as React from 'react';
 
 export default function Footer() {
   const socials = [
     {
       href: 'https://www.facebook.com/dianidreams',
-      name: 'meta',
+      name: 'bi-meta',
     },
     {
       href: 'https://www.instagram.com/dianidreams',
-      name: 'instagram',
+      name: 'bi-instagram',
     },
     {
       href: 'https://www.twitter.com/dianidreams',
-      name: 'twitter',
+      name: 'bi-twitter',
     },
   ];
   return (
@@ -29,7 +28,7 @@ export default function Footer() {
             {socials.map((soc, i) => (
               <li key={i}>
                 <a href={soc.href}>
-                  <box-icon type="logo" name={soc.name}></box-icon>
+                  <i className={soc.name} />
                 </a>
               </li>
             ))}
